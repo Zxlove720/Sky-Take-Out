b90fc4139bb72c318dd983daf6ab6af451452ea8
wzb <2482924813@qq.com>
2024-10-11 17:36:17 +0800

    Sky-Take-out start

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,6 +10,7 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -54,6 +56,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        // 此处传递的是DTO对象，为了方便封装前端提交的数据，但是传递给持久层（Mapper）还是建议使用实体类
+        // 所以说需要将EmployeeDTO对象转换为Employee对象（这两者不能直接进行类型转换）
+
+        /* Employee中的属性实际上是对EmployeeDTO属性的扩展，可以通过set/get对Employee对象的属性进行设置，
+           但二者的属性高度相似，这么写十分冗杂；所以说建议使用对象属性拷贝 */
+
+        // 对象属性拷贝
+        // ---- 将EmployeeDTO中和Employee相同的属性拷贝给Employee对象
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+        // Employee的属性比EmployeeDTO的属性更多，所以说拷贝之后需要手动设置Employee对象的属性
+
+
     }
 
 }
