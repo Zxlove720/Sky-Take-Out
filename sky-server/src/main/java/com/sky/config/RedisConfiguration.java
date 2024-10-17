f03f8db600da958c3f824d2b2fd22e2086388fc0
wzb <2482924813@qq.com>
2024-10-17 17:31:58 +0800

    config redis in java

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,5 +1,18 @@
 package com.sky.config;
 
+/* Java中可以操作Redis，常见的Redis的Java客户端：Jedis、Lettuce、Spring Data Redis，主要使用Spring Data Redis
+*  Spring Data Redis提供了一个高度封装的类：RedisTemplate；对相关API进行归类封装，将同一类型的操作封装为Operation接口
+*  ValueOperations：String数据操作
+*  SetOperations：set类型数据操作
+*  ZSetOperations：zset类型数据操作
+*  HashOperations：hash类型数据操作
+*  ListOperation：list类型数据操作
+* */
+
+// 环境准备
+// 1.在pom.xml文件中导入Spring Data Redis的maven坐标
+// 2.在.yml中配置Redis数据源：注意使用外部配置引用的方法配置
+// 3.编写配置类，创建RedisTemplate对象
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -20,5 +33,8 @@ public class RedisConfiguration {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
+    // 为什么要自己创建RedisTemplate类
+    // Spring boot框架的确会自动装配RedisTemplate对象，但是默认的key的序列化器为JdkSerializationRedSerializer，会导致
+    // 存储到Redis中的数据和原始数据有差别，所以说要手动设置StringRedisSerializer序列化器
 
 }
