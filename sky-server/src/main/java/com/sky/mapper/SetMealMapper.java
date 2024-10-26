de7f22982d9a229eead3835985467105de45d7e6
wzb <2482924813@qq.com>
2024-10-26 22:57:32 +0800

    insert new setMeal in SQL

--- sky-server/src/main/java/com/sky/mapper/SetMealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetMealMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealDTO;
 import com.sky.entity.Setmeal;
+import com.sky.enumeration.OperationType;
 import com.sky.vo.DishItemVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -55,5 +57,6 @@ public interface SetMealMapper {
     @Insert("insert into setmeal (id, category_Id, name, price, status, description, image, create_time, update_time, " +
             "create_user, update_user) values (#{id}, #{categoryId}, #{name}, #{price}, #{status}, #{description}, " +
             "#{image}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Setmeal setmeal);
 }
--- sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
@@ -2,8 +2,9 @@ package com.sky.service.impl;
 
 import com.sky.dto.SetmealDTO;
 import com.sky.entity.Setmeal;
-import com.sky.mapper.DishMapper;
+import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetMealMapper;
+import com.sky.mapper.SetmealDishMapper;
 import com.sky.service.SetMealService;
 import com.sky.vo.DishItemVO;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 @Service
 @Slf4j
@@ -19,6 +21,9 @@ public class SetMealServiceImpl implements SetMealService {
 
     @Autowired
     private SetMealMapper setMealMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
 
     /**
      * 根据菜品分类id查询套餐
@@ -56,8 +61,19 @@ public class SetMealServiceImpl implements SetMealService {
         // 因为有AOP编程，所以说不需要再为setmeal对象补充属性了
         // 直接调用Mapper中的方法添加新的套餐在数据库中
         setMealMapper.insert(setmeal);
-
+        // 需要将套餐和对应的菜品建立联系，所以说需要将套餐的id和其对应的菜品id添加到setmeal_dish表中
+        // 获取套餐的id
+        Long setmealId = setmeal.getId();
+        // 将套餐及其对应的菜品全部获得出来，添加到list中
+        List<SetmealDish> setmealDishs = setmealDTO.getSetmealDishes();
+        // 设置套餐及其菜品关系的套餐id
+        setmealDishs.forEach(new Consumer<SetmealDish>() {
+            @Override
+            public void accept(SetmealDish setmealDish) {
+                setmealDish.setSetmealId(setmealId);
+            }
+        });
+        // 批量保存套餐和菜品的关联关系
+        setmealDishMapper.insertBatch(setmealDishs);
     }
-
-
 }
